cc16118d3c9cb955b00b7b4f419c35ec90df2513
Michael Dietz <mjdietzx@gmail.com>
2015-12-20 10:46:15 -0600

    4.4.3.2 Implementing the LED characteristic.

--- ble_lbs.c
+++ ble_lbs.c
@@ -109,10 +109,63 @@ void ble_lbs_on_ble_evt(ble_lbs_t * p_lbs, ble_evt_t * p_ble_evt)
 }
 #endif // 0
 
+/**@brief Function for adding the LED characteristic.
+ *
+ * @param[in]   p_lbs        LED Button Service structure.
+ *
+ * @return      NRF_SUCCESS on success, otherwise an error code.
+ */
+static uint32_t led_char_add(ble_lbs_t * p_lbs)
+{
+    uint32_t            err_code;
+    ble_gatts_char_md_t char_md;
+    ble_gatts_attr_t    attr_char_value;
+    ble_uuid_t          ble_uuid;
+    ble_gatts_attr_md_t attr_md;
+
+    // Add LED characteristic
+    memset(&char_md, 0, sizeof(char_md));
+
+    char_md.char_props.read   = 1;
+    char_md.char_props.write  = 1;
+    char_md.p_char_user_desc  = NULL;
+    char_md.p_char_pf         = NULL;
+    char_md.p_user_desc_md    = NULL;
+    char_md.p_cccd_md         = NULL;
+    char_md.p_sccd_md         = NULL;
+
+    ble_uuid.type = p_lbs->uuid_type;
+		ble_uuid.uuid = LBS_UUID_LED_CHAR;
+
+    memset(&attr_md, 0, sizeof(attr_md));
+
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);
+		BLE_GAP_CONN_SEC_MODE_SET_NO_ACCESS(&attr_md.write_perm);
+    attr_md.vloc       = BLE_GATTS_VLOC_STACK;
+    attr_md.rd_auth    = 0;
+    attr_md.wr_auth    = 0;
+    attr_md.vlen       = 0;
+
+    memset(&attr_char_value, 0, sizeof(attr_char_value));
+
+    attr_char_value.p_uuid    = &ble_uuid;
+    attr_char_value.p_attr_md = &attr_md;
+    attr_char_value.init_len  = sizeof(uint8_t);
+    attr_char_value.init_offs = 0;
+    attr_char_value.max_len   = sizeof(uint8_t);
+    attr_char_value.p_value   = NULL;
+
+    if (err_code != NRF_SUCCESS)
+    {
+        return err_code;
+    }
+
+    return sd_ble_gatts_characteristic_add(p_lbs->service_handle, &char_md, &attr_char_value, &p_lbs->led_char_handles);
+}
+
 /**@brief Function for adding the Button characteristic.
  *
  * @param[in]   p_lbs        LED Button Service structure.
- * @param[in]   p_lbs_init   Information needed to initialize the service.
  *
  * @return      NRF_SUCCESS on success, otherwise an error code.
  */
@@ -174,7 +227,7 @@ uint32_t ble_lbs_init(ble_lbs_t * p_lbs, const ble_lbs_init_t * p_lbs_init)
 {
     uint32_t      err_code;
 	  ble_uuid128_t base_uuid = LBS_UUID_BASE;
-    ble_uuid_t ble_uuid;
+    ble_uuid_t    ble_uuid;
 
     // Initialize service structure
     p_lbs->conn_handle       = BLE_CONN_HANDLE_INVALID;
